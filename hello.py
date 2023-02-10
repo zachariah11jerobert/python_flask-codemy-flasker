0f44ecfd0e879e0a1c0c357b257e21c0be20a1e6
zachariah11jerobert <zachariah11jerobert@gmail.com>
2023-02-10 20:23:46 +0530

    done forms , flash messages , static assests

--- hello.py
+++ hello.py
@@ -1,44 +1,76 @@
-from flask import Flask , render_template
+from flask import Flask, render_template,flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "my super secret key"
+
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField()
+
+    ## ------------ MoreFields ---------------
+    # BooleanField  # DateField     # DataTimeField  # FileField    # HiddenField       # MultipleField
+    # FieldList     # Floatfield    # FormField      # IntegerField # Passwordfield     # RadioField
+    # SelectField   # SelectMultipleField   #SubmitField    # StringField   # TextAreaField      
+
+    ## ------------ Validators --------------
+    # DataRequired  # Email # EqualTo   # InputRequired # IPAddress # Length
+    # MacAddress    # NumberRange   # Optional  # Regexp    # URL   # AnyOf # NoneOf
+
 
 # Create a route decorater
-
-@app.route('/home')
+@app.route("/")
 def index():
     # -----------Filters--------
-    #safe
-    #capitalize
-    #lower
-    #upper
-    #title
-    #trim
-    #striptags
+    # safe  # capitalize    # lower # upper # title # trim  # striptags
 
-    firstName="John"
-    title="This is title text"
-    stuff="This is <strong>Bold</strong> Text"
+    firstName = "John"
+    title = "This is title text"
+    stuff = "This is <strong>Bold</strong> Text"
 
-    favouritePizza=["Pepperoni","Cheese Pizza","Hot Dog"]
-    return render_template("index.html",
-                    first_name=firstName,
-                    title=title,
-                    stuff=stuff,
-                    favourite_pizza=favouritePizza)
+    favouritePizza = ["Pepperoni", "Cheese Pizza", "Hot Dog"]
+
+    flash("Welcome To our Website!!!")
+    return render_template(
+        "index.html",
+        first_name=firstName,
+        title=title,
+        stuff=stuff,
+        favourite_pizza=favouritePizza,
+    )
+
 
 # localhost:5000/user/john
-@app.route('/user/<name>')
+@app.route("/user/<name>")
 def user(name):
-    return render_template("user.html",user_name=name)
+    return render_template("user.html", user_name=name)
+
 
 # Create Custom Error Pages
 # Inavalid URL
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"),404
+    return render_template("404.html"), 404
+
 
 # Internal Server Error
 @app.errorhandler(500)
 def page_not_found(e):
-    return render_template("500.html"),500
+    return render_template("500.html"), 500
+
+# Create Name Page
+@app.route('/name',methods=['GET','POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully!!!")
+    return render_template("name.html",name=name,form=form)
---
+++ static/js/myfile.js
@@ -0,0 +1,1 @@
+document.getElementById('demo').innerHTML = "This is created with javascript"
