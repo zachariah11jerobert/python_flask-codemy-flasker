90da4e42d45bb3a1a30e87ba7999617d53d925fa
zachariah11jerobert <zachariah11jerobert@gmail.com>
2023-02-11 06:24:13 +0530

    done database addition with sqlite

--- hello.py
+++ hello.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template,flash
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # Create a Flask Instance
 app = Flask(__name__)
+# Add Database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+# Secret Key!
 app.config["SECRET_KEY"] = "my super secret key"
+# Initialize The Database
+db = SQLAlchemy(app)
+
+
+# Create Model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # Crete A String
+    def __repr__(self):
+        return "<Name %r>" % self.name
+
+
+# Create a Form Class for user
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField()
 
 
 # Create a Form Class
@@ -16,11 +42,29 @@ class NamerForm(FlaskForm):
     ## ------------ MoreFields ---------------
     # BooleanField  # DateField     # DataTimeField  # FileField    # HiddenField       # MultipleField
     # FieldList     # Floatfield    # FormField      # IntegerField # Passwordfield     # RadioField
-    # SelectField   # SelectMultipleField   #SubmitField    # StringField   # TextAreaField      
+    # SelectField   # SelectMultipleField   #SubmitField    # StringField   # TextAreaField
 
     ## ------------ Validators --------------
     # DataRequired  # Email # EqualTo   # InputRequired # IPAddress # Length
     # MacAddress    # NumberRange   # Optional  # Regexp    # URL   # AnyOf # NoneOf
+
+
+@app.route("/user/add", methods=["GET", "POST"])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ""
+        form.email.data = ""
+        flash("User Added Successfully !!!")
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html", form=form, name=name, our_users=our_users)
 
 
 # Create a route decorater
@@ -63,14 +107,15 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template("500.html"), 500
 
+
 # Create Name Page
-@app.route('/name',methods=['GET','POST'])
+@app.route("/name", methods=["GET", "POST"])
 def name():
     name = None
     form = NamerForm()
     # Validate Form
     if form.validate_on_submit():
         name = form.name.data
-        form.name.data = ''
+        form.name.data = ""
         flash("Form Submitted Successfully!!!")
-    return render_template("name.html",name=name,form=form)
+    return render_template("name.html", name=name, form=form)
